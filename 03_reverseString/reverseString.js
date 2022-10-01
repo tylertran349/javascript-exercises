9f41d4c165fd7b8ba4d6f47cf826a3c862904c01
tylertran349 <tylertran349@gmail.com>
2022-09-30 21:40:51 -0700

    Add solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    let answer = "";
+    for(let i = inputStr.length - 1; i >= 0; i--) {
+        answer += inputStr[i];
+    }
+    return answer;
 };
 
 // Do not edit below this line
