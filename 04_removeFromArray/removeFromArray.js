cca4189165a121fef00dc6b67ed0776b7e3d1510
tylertran349 <tylertran349@gmail.com>
2022-09-30 22:41:11 -0700

    Add solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(nums, ...args) {
+   const answer = [];
+    for(let i = 0; i < nums.length; i++) {
+        if(!args.includes(nums[i])) {
+            answer.push(nums[i]);
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
