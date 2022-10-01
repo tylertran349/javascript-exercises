cdb507a8b00683b773483c7ddd65ef4022367c95
tylertran349 <tylertran349@gmail.com>
2022-09-30 23:05:49 -0700

    Add solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(typeof(start) === "string" || typeof(end) === "string" || typeof(start) === "object" || typeof(end) === "object") {
+        return 'ERROR';
+    } else if(start < 0 || end < 0) {
+        return 'ERROR';
+    } else {
+        let biggestNum = Math.max(start, end);
+        let smallestNum = Math.min(start, end);
+        for(let i = smallestNum; i <= biggestNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
