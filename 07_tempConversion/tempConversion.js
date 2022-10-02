6ed8b59bd5465de81ac67eea0db810eb1d1e1045
tylertran349 <tylertran349@gmail.com>
2022-10-01 21:47:22 -0700

    Add solution for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let answer = (temp - 32) * (5.0/9);
+  return Math.round(answer * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let answer = (temp * (9.0/5) + 32);
+  return Math.round(answer * 10)/10;
 };
 
 // Do not edit below this line
