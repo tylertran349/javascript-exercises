37de2a801316b3913c159bcbe8d5bfcf330f8271
tylertran349 <tylertran349@gmail.com>
2022-10-17 22:40:38 -0700

    Add solution for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let newText = text.toLowerCase().replace(/[^A-Za-z]/g, "");
+    let reversedText = newText.split("").reverse().join("");
+    if(reversedText === newText) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
